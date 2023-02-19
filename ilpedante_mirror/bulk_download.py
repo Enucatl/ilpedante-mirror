ad893309a20001d68a86aa3148208e12039fd7f8
Matteo Abis <1423701+Enucatl@users.noreply.github.com>
2023-02-19 14:39:19 +0100

    update for new version of the blog

--- ilpedante_mirror/bulk_download.py
+++ ilpedante_mirror/bulk_download.py
@@ -11,69 +11,50 @@ import requests
 locale.setlocale(locale.LC_ALL, "it_IT.UTF-8")
 
 
-def find_next(url):
-    # find the link to the next pagination page
-    page = requests.get(url).content
-    soup = BeautifulSoup(page, features="html.parser")
-    next_page = soup.find("li", class_="next").a.get("href")
-    return next_page
-
-
 def parse_link(row):
     # parse the links in the blog main page to get the links to the posts and
     # their metadata
-    post_previews = row["soup"].find_all(class_="post-preview")
+    post_previews = row["soup"].find_all("article", class_="articoli-item")
+
     posts = [
         {
-            "url": post_preview.a.get("href"),
-            "title": post_preview.find("h2", class_="post-title").text.strip(),
-            "subtitle": post_preview.find("h3", class_="post-subtitle").text.strip(),
-            "meta": post_preview.find("p", class_="post-meta").text,
+            "url": post_preview.find("h1", class_="titolo").a.get("href"),
+            "title": post_preview.find("h1", class_="titolo").text.strip(),
+            "author": post_preview.find("meta", itemprop="author").get("content"),
+            "date": post_preview.find("meta", itemprop="datePublished").get("content"),
         }
         for post_preview in post_previews
     ]
     df = pd.DataFrame.from_records(posts)
-    df["date"] = pd.to_datetime(
-        df["meta"].str.split("|").str[0].str.strip(), format="%d %B, %Y"
-    )
-    df["comments"] = (
-        df["meta"]
-        .str.split("|")
-        .str[1]
-        .str.replace("Nessun", "0")
-        .str.replace("commento", "commenti")
-        .str.replace("commenti", "")
-        .astype(int)
-    )
-    df["pedanteria"] = df["meta"].str.split("|").str[2].str.count("●")
+    df["date"] = pd.to_datetime(df["date"])
     return df
 
 
 def parse_post(row):
     # get the actual post contents
-    row["title"] = row["soup"].find("h1").string
-    article = row["soup"].find("article").div.div.div
-    article.find("div", class_="share").decompose()
+    article = row["soup"].find("div", itemprop="articleBody")
     row["post"] = article.prettify()
     row["post_markdown"] = markdownify(row["post"])
     return row
 
 
+def page_url(root_url: str, i: int) -> str:
+    return f"{root_url}/{i}"
+
+
+def page_generator(root_url):
+    page = 1
+    while (page_content := requests.get(page_url(root_url, page)).content):
+        soup = BeautifulSoup(page_content, features="html.parser")
+        page += 1
+        yield soup
+
+
 @click.command()
-@click.argument("root_url", default="http://ilpedante.info/blog/page-0")
+@click.argument("root_url", default="http://ilpedante.info/home/loadArticoli")
 def main(root_url):
-    urls = [root_url]
-    next_url = root_url
-    while True:
-        try:
-            next_url = find_next(next_url)
-            urls.append(next_url)
-        except AttributeError:  # there was no next so this is the last page
-            break
-    df = pd.DataFrame({"url": urls})
-    # get all pages of the blog
-    df["html"] = df["url"].apply(lambda x: requests.get(x).content)
-    df["soup"] = df["html"].apply(BeautifulSoup, features="html.parser")
+    soups = [soup for soup in page_generator(root_url)]
+    df = pd.DataFrame({"soup": soups})
     df = df.apply(parse_link, axis=1)
     df = pd.concat(df.tolist())
     # get all articles from the collected links
